9c0622845972ac3fb9f86d449bdd7d488bc2adea
meilixiaozhang <redpangci@gmail.com>
2020-03-27 22:52:38 +1100

    render the website with api

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
+
 
 const app = express();
 
@@ -13,12 +15,54 @@ app.get("/", function(req, res){
 });
 
 app.post("/", function(req, res) {
-  var firstName = req.body.fName;
-  var lastName = req.body.lName;
-  var email = req.body.email;
-  console.log(firstName, lastName, email);
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+  const jsonData = JSON.stringify(data);
+  const url = "https://us19.api.mailchimp.com/3.0/lists/7023cce9c3";
+  const options = {
+    method: "POST",
+    auth: "xiaozhang:4c103c0386597810ad7103a6dc4691c5-us1"
+  }
+  const request = https.request(url, options, function(response){
+
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname+"/success.html")
+    } else {
+      res.sendFile(__dirname+"/failure.html")
+    }
+    response.on("data",function(data) {
+      console.log(JSON.parse(data));
+    })
+  })
+  request.write(jsonData);
+  request.end();
+})
+
+app.post("/failure", function(req,res){
+  res.redirect("/");
 })
 
 app.listen(3000, function(){
   console.log("Server running on port 3000.")
 })
+
+
+
+
+// Api key
+// 4c103c0386597810ad7103a6dc4691c5-us19
+// List id
+// 7023cce9c3
