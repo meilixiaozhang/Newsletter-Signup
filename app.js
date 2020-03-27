3470b33767baae6cc12a66885150a46b261b6dde
meilixiaozhang <redpangci@gmail.com>
2020-03-27 23:41:00 +1100

    remove cool api

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
-const cool = require("cool-ascii-faces");
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 const https = require("https");
-
-
 
 const app = express();
 
 app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({extended:true}));
-
-app.get('/cool', (req, res) => res.send(cool()));
 
 app.get("/", function(req, res){
   res.sendFile(__dirname+"/signup.html")
